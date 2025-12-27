aa8d5f5cb1818cc2e5a96150723ddcccbb50a1d4
mathfajardo <mathfajardo.contato@hotmail.com>
2025-12-27 17:54:25 -0300

    deploy

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import './assets/global.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
